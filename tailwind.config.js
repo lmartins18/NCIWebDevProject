ff3f9021c968a975f105c2b745bc432706b8125a
Luis Martins <luismwolf@gmail.com>
2023-11-22 13:11:51 -0500

    change: add tailwind remove bootstrap

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+      },
+      animation: {
+        'gradient-fade': 'gradientFade 5s ease-in-out infinite',
+        'slide-in-below': '2s ease-out 0s 1 slideInBelow'
+      },
+      backgroundSize: {
+        '200' : '200% 200%'
+      }
+    },
+  },
+  plugins: [],
+};
